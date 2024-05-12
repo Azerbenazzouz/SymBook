8c0072835f0cff47f138ef273d703810cecfadb9
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-05-12 23:40:33 +0100

    Pagination 🤩❤️

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,24 +4,44 @@ namespace App\Controller;
 
 use App\Repository\CategoriesRepository;
 use App\Repository\LivresRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    // #[Route('/', name: 'app_home')]
+    // public function index(LivresRepository $rep,CategoriesRepository $rep1): Response
+    // {
+    //     // show the last 8 new books
+    //     $livres = $rep->findBy([], ['editedAt' => 'DESC'], 8);
+    //     $categories = $rep1->findAll();
+    //     return $this->render('home/index.html.twig', [
+    //         'controller_name' => 'HomeController',
+    //         'livres' => $livres,
+    //         'categories' => $categories
+    //     ]);
+    // }
+
     #[Route('/', name: 'app_home')]
-    public function index(LivresRepository $rep,CategoriesRepository $rep1): Response
-    {
-        // show the last 8 new books
-        $livres = $rep->findBy([], ['editedAt' => 'DESC'], 8);
-        $categories = $rep1->findAll();
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'livres' => $livres,
-            'categories' => $categories
-        ]);
-    }
+     public function livre(LivresRepository $rep,PaginatorInterface $paginateur, Request $request,CategoriesRepository $rep1): Response
+     {
+         $data = $rep->findAll();
+         $livres=$paginateur->paginate(
+            $data,
+            $request->query->getInt('page',1),
+            8
+         );
+
+         $categories = $rep1->findAll();
+
+         return $this->render('home/index.html.twig', [
+             'livres' => $livres,
+             'categories' => $categories
+         ]);
+     }
     // tous les livres
     #[Route('/livres', name: 'app_livres')]
     public function livres(LivresRepository $rep): Response
@@ -33,7 +53,7 @@ class HomeController extends AbstractController
     }
     // livre par id
     #[Route('/livre/{id}', name: 'app_livre')]
-    public function livre(LivresRepository $rep, $id): Response
+    public function livreId(LivresRepository $rep, $id): Response
     {
         $livre = $rep->find($id);
 
