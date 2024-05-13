87e8286b0a3545dbe0c91a33900ffcc38613504e
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-05-13 01:13:36 +0100

    chore: Update UserController to include user update functionality

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,8 +11,20 @@ class UserController extends AbstractController
     #[Route('/user', name: 'app_user')]
     public function index(): Response
     {
+        $user = $this->getUser();
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+            'user' => $user,
+        ]);
+    }
+    // update the user from the form
+    #[Route('/user/update', name: 'app_user_update')]
+    public function update(): Response
+    {
+        $user = $this->getUser();
+        return $this->render('user/update.html.twig', [
+            'controller_name' => 'UserController',
+            // 'user' => $user,
         ]);
     }
 }
