9d5ed5c9c9aff6b42715567e3dab4a9cf98e89a3
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-05-20 10:01:26 +0100

    chart

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,17 +11,20 @@ use App\Repository\UserRepository;
 use DateInterval;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 #[IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
 
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(OrderRepository $orderRepository,UserRepository $userRepository,OrderDetailsRepository $orderDetailsRepository,LivresRepository $livresRepository): Response
+    public function index(OrderRepository $orderRepository,UserRepository $userRepository,OrderDetailsRepository $orderDetailsRepository,LivresRepository $livresRepository,EntityManagerInterface $em): Response
     {
         $today = new DateTime();
       
@@ -96,11 +99,110 @@ class AdminController extends AbstractController
             'totalUsers' => $totalUsers, 
             'totalOrders' => $totalOrders,
             'mostOrderedBook' => $mostOrderedBook,
-    'lastOrderDate' => $lastOrderDate,
-    'dayOfWeek' => (new \DateTime())->format('l'),
-          
-           
+            'lastOrderDate' => $lastOrderDate,
+            'dayOfWeek' => (new \DateTime())->format('l'),
+            'orders'=> $orders,
+
+            // 'salesChart' => $this->chartSales($orderRepository,$em),
         ]);
     }
+
+    // chart sales 
+    #[Route('/chart/sales', name: 'app_chart_sales')]
+    public function chartSales(OrderRepository $orderRepository,EntityManagerInterface $em): Response
+    {
+        // $orders = $orderRepository->createQueryBuilder('o')
+        //     ->select('DATE(o.create_at) AS date, SUM(o.total) AS total')
+        //     ->getQuery()
+        //     ->getResult();
+        
+        // use Doctrine\ORM\Query\ResultSetMapping;
+        //  $ordersData = $orderRepository->findAll();
+        // $orders = [];
+
+        // foreach ($ordersData as $order) {
+        //     // $total = $order->getTotal();
+        //     $orders[] = [
+        //         'date' => $order->getCreateAt()->format('Y-m-d'),
+        //         'total' => $order->getTotal(),
+        //     ];
+        // }
+
+
+        // $rsm = new ResultSetMapping();
+        // $rsm->addScalarResult('date', 'date');
+        // $rsm->addScalarResult('total', 'total');
+        
+        // $query = $em->createNativeQuery('SELECT DATE(o.create_at) AS date, SUM(o.total) AS total FROM order o GROUP BY date', $rsm);
+        // $orders = $query->getResult();
+        // $em = $this->$this->getDoctrine();()->getManager();
+        // $query = $em->createNativeQuery("SELECT DATE(o.create_at) AS date, SUM(o.total) AS total FROM orders o GROUP BY date")->getResult();
+        // $orders = $query->getResult();
+
+        $data = [];
+        // foreach ($orders as $order) {
+        //     $data[] = [
+        //         'date' => $order['date'],
+        //         'total' => $order['total'],
+        //     ];
+        // }
+
+        return $this->json($data);
+    }
+
+    #[Route('/chartjs', name: 'app_chartjs')]
+    public function __invoke(ChartBuilderInterface $chartBuilder): Response
+    {
+        // $package = $packageRepository->find('chartjs');
+
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+
+
+        return $this->render('admin/_sales_chart.html.twig', [
+            // 'package' => $package,
+            'chart' => $chart,
+        ]);
+    }
+
 }
 
